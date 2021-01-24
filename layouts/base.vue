cf8cd4f5f54b379336061540e6535e5c7dee2ec4
Hiro Degawa <hxdegawa@gmail.com>
2021-01-24 18:23:58 +0900

    initial commit

---
+++ layouts/base.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .layout
+    nuxt
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({})
+export default class BaseLayout extends Vue {
+  mounted() {
+    let vh: number = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+    window.addEventListener('resize', () => {
+      vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+.layout {
+  position: relative;
+  display: block;
+  background-color: $color-bg-primary;
+  min-height: calc(var(--vh, 1vh) * 100);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .top
+    p Hello, World!
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  layout: 'base',
+})
+export default class IndexPage extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.top {
+  @include page-base;
+}
+</style>
